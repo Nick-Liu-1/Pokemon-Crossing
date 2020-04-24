e90810ef53428f06e5a07791d183592e60ff7756
Annie Zhang <55457552+anniezhang31@users.noreply.github.com>
2020-04-23 23:15:28 -0400

    Items

--- Assets/Rooms/HouseTileset.tsx
+++ Assets/Rooms/HouseTileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.1" name="HouseTileset" tilewidth="16" tileheight="16" tilecount="12288" columns="192">
- <image source="Game Boy Advance - Pokemon Emerald - Interior Tilesets.png" trans="003039" width="3072" height="1024"/>
+<tileset version="1.2" tiledversion="1.3.1" name="HouseTileset" tilewidth="16" tileheight="16" tilecount="12288" columns="120">
+ <image source="Game Boy Advance - Pokemon Emerald - Interior Tilesets.png" trans="003039" width="1920" height="640"/>
 </tileset>
