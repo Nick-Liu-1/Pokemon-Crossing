bde0cc2089a0b2315d720f2c76994b621a5116c1
Annie Zhang <55457552+anniezhang31@users.noreply.github.com>
2020-05-19 16:44:34 -0400

    room fixes

--- src/Assets/resizer.py
+++ src/Assets/resizer.py
@@ -1,13 +1,19 @@
 from PIL import Image
 import glob
 
-basewidth = 1920
-
-for file in glob.glob("C:\\Users\\nickl\\Desktop\\Com Sci\\Pokemon Crossing\\Assets\\Rooms\\*.png"):
+def resize(file, basewidth):
     img = Image.open(file)
     wpercent = (basewidth/float(img.size[0]))
     hsize = int((float(img.size[1])*float(wpercent)))
     img = img.resize((basewidth,hsize), Image.ANTIALIAS)
     img.save(file)
+    print("done")
 
-print("done")
+def resizeFolder(folder, baseWidth):
+    for file in glob.glob(folder):
+        resize(file, baseWidth)
+    print("done all")
+
+
+
+
