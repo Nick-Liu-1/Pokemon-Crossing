7a3552777d97386c7e20b3a975dbaf8152c34338
Annie Zhang <55457552+anniezhang31@users.noreply.github.com>
2020-06-05 12:54:19 -0400

    loop!!!

--- src/MP3.java
+++ src/MP3.java
@@ -26,68 +26,35 @@ import javazoom.jl.player.Player;
 public class MP3 {
     private String filename;
     private Player player; 
+    private boolean loop = true;
 
     // constructor that takes the name of an MP3 file
     public MP3(String filename) {
         this.filename = filename;
     }
-
-    public void close() { if (player != null) 
-    	player.close(); }
-
-    // play the MP3 file to the sound card
+    
     public void play() {
-        try {
-        	FileInputStream fis     = new FileInputStream(filename);
-            BufferedInputStream bis = new BufferedInputStream(fis);
-            player = new Player(bis);            	
-        }
-     
-        catch (Exception e) {
-            System.out.println("Problem playing file " + filename);
-            System.out.println(e);
-        }
-
-        // run in new thread to play in background
-        new Thread() {
+    	new Thread() {	//plays in background
             public void run() {
-                try { 
-                	while(true){
-                		player.play(); 
-                	}
-                }
-                catch (Exception e) { System.out.println(e); }
+		        try {
+		            do {
+		                FileInputStream fis     = new FileInputStream(filename);
+		            	BufferedInputStream bis = new BufferedInputStream(fis);
+		            	player = new Player(bis); 
+		                player.play();
+		            } while (loop);		//to loop music
+		        }
+		        catch (Exception e) {
+		        	System.out.println("Problem playing file " + filename);
+		            System.out.println(e);
+		        }
             }
-        }.start();
-
-
-
-
+		}.start();
     }
 
-
-   /* // test client
-    public static void main(String[] args) {
-        MP3 mp3 = new MP3("Assets/Sounds/Bubblegum.mp3");
-        mp3.play();
-
-        // do whatever computation you like, while music plays
-        int N = 800;
-        double sum = 0.0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                sum += Math.sin(i + j);
-            }
-        }
-        System.out.println(sum);
-
-        // when the computation is done, stop playing it
-        mp3.close();
-
-        // play from the beginning
-        mp3 = new MP3("Assets/Sounds/Bubblegum.mp3");
-        mp3.play();
-
-    }*/
-
+    public void close(){
+        loop = false;
+        player.close();
+    }
+    
 }
